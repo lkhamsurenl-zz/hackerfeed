c79c3684f6addf4c3a46150def2e0ab0abf3146a
Luvsandondov Lkhamsuren <lkhamsurenl@gmail.com>
2015-12-26 11:32:46 -0800

    Initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+// Load the index.html page upon entering.
+chrome.app.runtime.onLaunched.addListener(function() {
+  chrome.app.window.create('index.html', 
+    { "innerBounds": { "width": 400, "height": 300 },
+      "id": "index"
+    });
+});
