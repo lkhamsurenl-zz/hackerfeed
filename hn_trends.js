ab1a75171734f9d2bb536579121be394a09a6984
Luvsandondov Lkhamsuren <lkhamsurenl@gmail.com>
2015-12-28 22:35:54 -0800

    initial version of HN

--- hn_trends.js
+++ hn_trends.js
@@ -1,19 +1,21 @@
-// Compare 2 stories based on the scores.
-function compareStories(s1, s2) {
-    if (s1["score"] < s2["score"])
-        return -1;
-    if (s1["score"] > s2["score"])
-        return 1;
-    return 0;
+// Given items, display items in popup.html.
+function render(item) {
+    url = item["url"];
+    title = item["title"];
+    link = "<a href=" + url + ">" + 
+        title + "</a> <br />";
+    // Display in the trends paragraph in popup.html page.
+    document.getElementById("trends").innerHTML += link;
 }
 
-function getStory(id, desc_items) {
+function getStory(id) {
     var xmlhttp = new XMLHttpRequest();
 
     xmlhttp.onreadystatechange = function() {
         // if the request succeed, add the json value to the desc_items list.
         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-            desc_items.push(JSON.parse(xmlhttp.responseText));
+            var obj = JSON.parse(xmlhttp.responseText);
+            render(obj);
         }
     };
     xmlhttp.open("GET", "https://hacker-news.firebaseio.com/v0/item/" + 
@@ -23,28 +25,11 @@ function getStory(id, desc_items) {
 }
 
 // Get only top limit # of stories from jsonObj.
-function limitTopStories(jsonObj, limit) {
-    var items = jsonObj.items.slice(0, limit * 2);
-    var desc_items = [];
-    for (i = 0; i < items.length; i++) {
-        getStory(items[i], desc_items);
+function limitTopStories(items, limit) {
+    var limited_items = items.slice(0, limit);
+    for (i = 0; i < limited_items.length; i++) {
+        getStory(items[i]);
     }
-    desc_items.sort(compareStories);
-    return desc_items.slice(0, limit);
-}
-
-// Given items, display items in popup.html.
-function render(items) {
-	var links = "<center><h3>HN</h3></center>";
-    jQuery.each(items, function(i, item) {
-    	url = item["url"];
-    	title = item["title"];
-    	links += i + ". ";
-    	links += "<a href=" + url + ">" + 
-    			 title + "</a> <br />";
-    });
-	// Display in the trends paragraph in popup.html page.
-	document.getElementById("trends").innerHTML += links;
 }
 
 // function to make API call to url and display top limit results.
@@ -54,14 +39,15 @@ function callAPI(limit) {
     xmlhttp.onreadystatechange = function() {
         // if the request succeed, display the top requests.
         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-            var items = limitTopStories(JSON.parse(xmlhttp.responseText), limit)
-            render(items);
+            // Display in the trends paragraph in popup.html page.
+            document.getElementById("trends").innerHTML += "<center><h3>HN</h3></center>";
+            limitTopStories(JSON.parse(xmlhttp.responseText), limit)
         }
     };
     
-    xmlhttp.open("GET", "https://hacker-news.firebaseio.com/v0/newstories");
+    xmlhttp.open("GET", "https://hacker-news.firebaseio.com/v0/newstories.json");
     xmlhttp.send();
 }
 
-// Popular repos created within last 7 days.
+// Popular 15 stories.
 callAPI(15);
