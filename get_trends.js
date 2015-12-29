9b7ba02625a61e16e6cd2c673ee156478f7318e7
Luvsandondov Lkhamsuren <lkhamsurenl@gmail.com>
2015-12-28 17:54:18 -0800

    Display ML related repos

--- get_trends.js
+++ get_trends.js
@@ -1,24 +1,28 @@
 // Format given date to YYYY-MM-DD.
 function shortFormatDate(date) {
-	day = date.getDate();
-	month = date.getMonth() + 1; // Month is 0 based.
+	day = ('0' + date.getDate()).slice(-2); // Put leading 0 if necessary.
+	month = ('0' + (date.getMonth() + 1)).slice(-2); // Month is 0 based.
 	year = date.getFullYear();
 
 	return year + "-" + month + "-" + day;
 }
 
 function subtractedDaysFromCurrent(num_days) {
+    // Get current date.
 	var start_date = new Date();
-	start_date.setDate(start_date.getDate() - num_days);
+    // Subtract num_days from current date.
+	start_date.setTime(start_date.getTime() - num_days*24*60*60*1000);
+
 	return start_date;
 }
 
-// Given a Json Object with top trends, parse and render in popup.html page.
-function parseDescriptions(jsonObj) {
+// Given a Json Object with top limit trends, parse and render in popup.html
+// page.
+function parseDescriptions(title, jsonObj, limit) {
 	// Get description and html_url for each entry.
 	// TODO(lkhamsurenl): Show only English repositories.
-	var links = "";
-    jQuery.each(jsonObj.items, function(i, item) {
+	var links = "<center><h3>" + title + "</h3></center>";
+    jQuery.each(jsonObj.items.slice(0, limit), function(i, item) {
     	url = item["html_url"];
     	description = item["description"];
     	language = item["language"];
@@ -32,24 +36,45 @@ function parseDescriptions(jsonObj) {
     			 " [" + language + "] <br />";
     });
 	// Display in the trends paragraph in popup.html page.
-	document.getElementById("trends").innerHTML = links;
+	document.getElementById("trends").innerHTML += links;
 }
 
-// TODO(lkhamsurenl): Cache results for one day, load when it's too old.
-var xmlhttp = new XMLHttpRequest();
+// function to make API call to url and display top limit results.
+function makeAPIcall(title, url, limit) {
+    // TODO(lkhamsurenl): Cache results for one day, load when it's too old.
+    var xmlhttp = new XMLHttpRequest();
 
-xmlhttp.onreadystatechange = function() {
-	// if the request succeed, display the top requests.
-    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-        parseDescriptions(JSON.parse(xmlhttp.responseText));
-    }
-};
+    xmlhttp.onreadystatechange = function() {
+        // if the request succeed, display the top requests.
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            parseDescriptions(title, JSON.parse(xmlhttp.responseText), limit);
+        }
+    };
 
-// Get a date in short_format 7 days from current date.
-var short_date = shortFormatDate(subtractedDaysFromCurrent(7));
+    
+    xmlhttp.open("GET", url);
+    xmlhttp.send();
+}
 
-// Make API call to get the top repositories created since short_date.
-xmlhttp.open("GET", "https://api.github.com/search/repositories?q=created:>" + 
-	short_date + 
-	"&sort=stars&order=desc");
-xmlhttp.send();
+// Get Popular repos created within days with keyword, display only top limit
+// results.
+// "" keyword is a default value.
+function getTrendingByKeywords(keyword, days, limit) {
+    
+    var short_date = shortFormatDate(subtractedDaysFromCurrent(days));
+
+    var url = "https://api.github.com/search/repositories?q=" + 
+        keyword +
+        " created:>" + 
+        short_date + 
+        "&sort=stars&order=desc";
+
+    var title = (keyword == "" ? "Recent top repositories" : keyword + " ");
+    makeAPIcall(title, url, limit);
+}
+
+// Popular repos created within last 7 days.
+getTrendingByKeywords("", 7, 15)
+
+// Popular ML repos created within last month.
+getTrendingByKeywords("Machine Learning", 30, 15);
