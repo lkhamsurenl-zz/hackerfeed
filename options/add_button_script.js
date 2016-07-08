e92332f15eaa14ce2a74acd9fea18a687f467f19
Enkh-Ider Jargalsaikhan <jargalsaikhane@gmail.com>
2016-07-08 00:02:58 -0500

    small style changes

--- options/add_button_script.js
+++ options/add_button_script.js
@@ -1,5 +1,4 @@
 // Open Options page when user clicks on the add button.
-var button = document.getElementById("add_button");
-button.addEventListener("click", function(e){
+$("#add_button").click(function(){
 	chrome.runtime.openOptionsPage();
-}, false);
+});
